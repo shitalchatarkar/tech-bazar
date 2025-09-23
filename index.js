ecff793ffeb1885431a909f2357b5fb70ad3f24a
shital chatarkar <shitalchatarkar21@gmail.com>
2025-09-23 18:57:54 +0530

    ui

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const { usermiddleware } = require("./middleware/user.middleware")
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: "https://tech-bazar-idfl.onrender.com", credentials: true }))
 
 app.use(express.static("dist"))
 
@@ -26,8 +27,8 @@ app.use((req, res, next) => {
     console.log(req.method);
     console.log(req.url);
 
-    // res.sendFile(path.join(__dirname, "dist", "index.html"))
-    res.status(404).json({ message: "Route not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    // res.status(404).json({ message: "Route not found" })
 })
 app.use((err, req, res, next) => {
     console.error(err)
