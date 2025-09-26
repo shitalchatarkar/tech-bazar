2a1c31c0d69d449442033f38947404d2662c65f4
shital chatarkar <shitalchatarkar21@gmail.com>
2025-09-26 15:56:28 +0530

    email update

--- utils/email.js
+++ utils/email.js
@@ -2,9 +2,7 @@ const nodemailer = require("nodemailer")
 const sendEmail = ({ to, subject, message }) => new Promise(async (resolve, reject) => {
     try {
         const transport = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
+            service: "gmail",
             auth: { user: process.env.EMAIL, pass: process.env.PASS }
         })
         await transport.sendMail({ to, subject, text: message })
